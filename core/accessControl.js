b402f3d536e8aeeafd87aad386c738fe9a0e7362
ghqs <739683017@qq.com>
2022-05-03 23:24:21 +0800

    update

---
+++ core/accessControl.js
@@ -0,0 +1,32 @@
+function loop() {
+
+	if(window.location.pathname.indexOf('blog%2Fletter') != -1){
+
+		var psw = ""
+
+		while ( btoa(psw) != "Z2hxcw==") { //设置密码
+
+			psw = prompt("口令")	
+
+		}
+
+	}
+}
+
+//url变化的监听器
+if( ('onhashchange' in window) && ((typeof document.documentMode==='undefined') || document.documentMode==8)) {
+    // 浏览器支持onhashchange事件
+    window.onhashchange = loop;  // TODO，对应新的hash执行的操作函数
+} else {
+    // 不支持则用定时器检测的办法
+    setInterval(function() {
+        // 检测hash值或其中某一段是否更改的函数， 在低版本的iE浏览器中通过window.location.hash取出的指和其它的浏览器不同，要注意
+        var ischanged = isHashChanged();
+        if(ischanged) {
+            loop();  // TODO，对应新的hash执行的操作函数
+        }
+    }, 150);
+}
+
+
+
